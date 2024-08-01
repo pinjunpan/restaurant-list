f678470577e0cf35367e207e7cf05bfe78561ba2
pinjunpan <pinjunpann@gmail.com>
2024-08-01 22:55:16 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.get('/restaurants', (req, res) => {
 
 app.get('/restaurants/:id', (req, res) => {
   const id = req.params.id
-  res.send(`read restaurant: ${id}`)
+  const restaurant = restaurants.find((restaurant) => restaurant.id.toString() === id)
+  res.render('detail', {restaurant})
 })
 
 app.listen(port, () => {
