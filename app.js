d080f716adade39cc5e307df2ad682d81b3bd349
pinjunpan <pinjunpann@gmail.com>
2024-10-10 17:37:22 +0800

    install & initialize sequelize

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express()
 const port = 3000
 const restaurants = require('./public/jsons/restaurant.json').results
 
+const db = require('./models')
+const Restaurant = db.Restaurant
+
 app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', './views');
