2182515b9a9b1a3155b955340237cbbc2386c988
pinjunpan <pinjunpann@gmail.com>
2024-11-08 17:37:41 +0800

    add salting and encrypting while accessing password

--- seeders/20241010143144-initial-data.js
+++ seeders/20241010143144-initial-data.js
@@ -1,29 +1,53 @@
 'use strict'
-
+const bcrypt = require('bcryptjs')
 const restaurants = require('../public/jsons/restaurant.json').results
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Restaurants', restaurants.map((restaurant) =>
-      ({
-        name: restaurant.name,
-        name_en: restaurant.name_en,
-        category: restaurant.category,
-        image: restaurant.image,
-        location: restaurant.location,
-        phone: restaurant.phone,
-        google_map: restaurant.google_map,
-        rating: restaurant.rating,
-        description: restaurant.description,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      })
-    )
-    )
+    let transaction
+
+    try {
+      transaction = await queryInterface.sequelize.transaction()
+
+      const hash = await bcrypt.hash('12345678', 10)
+
+      await queryInterface.bulkInsert('Users', [
+        {
+          id: 1,
+          name: 'user1',
+          email: 'user1@example.com',
+          password: hash,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },{
+          id: 2,
+          name: 'user2',
+          email: 'user2@example.com',
+          password: hash,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }], { transaction }
+      )
+      await queryInterface.bulkInsert('Restaurants', restaurants.map((restaurant, i) => {
+        const userId = i < 4 ? 1 : 2
+          return {
+            ...restaurant,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            userId: userId
+          }
+      }), { transaction }
+      )
+
+      await transaction.commit()
+    } catch (error) {
+      console.log(error)
+      if (transaction) await transaction.rollback()
+    }
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Restaurants', null)
+    await queryInterface.bulkDelete('Users', null)
   }
 }
